de4a4afd68eb23bb65da5ae5dd9f468f1787af84
Shelly Tang <shellytang@gmail.com>
2017-01-18 15:49:05 -0800

    updated index.html and updated repoView.js and repo.js

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -7,8 +7,25 @@
 
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-
+    $.ajax({
+      url: 'https:api.github.com/users/shellytang/repos?type=owner',
+      method: 'GET',
+      headers: {
+        Authorization: `token ${githubToken}`
+        // Authorization: 'token ' + githubToken
+      }
+    })
+    .then(data => {
+      repos.all = data;
+      callback();
+    })
+    // .then(function(data){
+    //   repos.all = data;
+    //   console.log(data);
+    //   callback();
+    // })
   };
+
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
